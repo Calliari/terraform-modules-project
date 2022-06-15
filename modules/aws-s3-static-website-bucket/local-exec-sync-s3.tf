2e5f7af6b46f225c6fcea94864ee541b22474175
Calliari <jrcalliari@me.com>
2022-06-15 20:53:15 +0100

    output for module aws_s3_bucket fixed

--- modules/aws-s3-static-website-bucket/local-exec-sync-s3.tf
+++
@@ -1,5 +0,0 @@
-resource "null_resource" "cmd_cp_to_s3" {
-  provisioner "local-exec" {
-    command = "aws s3 --profile ${var.aws_profile} cp modules/aws-s3-static-website-bucket/www/ s3://${aws_s3_bucket.this.id}/ --recursive"
-  }
-}
--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,22 @@ output "all" {
   value       = aws_s3_bucket.this.*
 }
 
+output "arn" {
+  description = "ARN of the bucket"
+  value       = aws_s3_bucket.this.arn
+}
+
+output "name" {
+  description = "Name (id) of the bucket"
+  value       = aws_s3_bucket.this.id
+}
+
+output "domain" {
+  description = "Domain name of the bucket"
+  value       = aws_s3_bucket_website_configuration.this.website_domain
+}
+
+output "endpoint" {
+  description = "Name (endpoint) of the bucket"
+  value       = aws_s3_bucket_website_configuration.this.website_endpoint
+}
