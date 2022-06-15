c9b1681a6832ee3dd39dfa902872845334468973
Calliari <jrcalliari@me.com>
2022-06-15 09:36:01 +0100

    Adding a lock for tf version within tf block config

--- provider.tf
+++ provider.tf
@@ -1,7 +1,18 @@
 ##################################################################################
 # TERRAFORM CONFIG
 ##################################################################################
+## Uncomment this block to use Terraform Cloud
+# cloud {
+#   organization = "organization-name"
+#   workspaces {
+#     name = "learn-terraform-module-use"
+#   }
+# }
+
+# This Terraform configuration requires a use of Terraform version greater than v1.1.0.
+# This Terraform configuration must also use the aws provider that is within the v4.18.0 minor release. 
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,9 +21,8 @@ terraform {
   }
 }
 
-
 ##################################################################################
-# PROVIDERS
+# PROVIDERS 
 ##################################################################################
 provider "aws" {
   profile = var.profile
