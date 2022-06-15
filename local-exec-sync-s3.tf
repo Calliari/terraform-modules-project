dc8a45085b5e5eeec85de0fa2f34a0581a03b437
Calliari <jrcalliari@me.com>
2022-06-15 20:11:34 +0100

    fixing the path for sync the files into s3 bkt

--- local-exec-sync-s3.tf
+++ local-exec-sync-s3.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "cmd_cp_to_s3" {
   provisioner "local-exec" {
-    command = "aws s3 --profile ${var.aws_profile} cp ./www/ s3://${aws_s3_bucket.this.id}/ --recursive"
+    command = "aws s3 --profile ${var.aws_profile} cp .terraform/modules/website_s3_bucket/www/ s3://${aws_s3_bucket.this.id}/ --recursive"
   }
 }
