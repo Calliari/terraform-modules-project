7e954aee4ef5e23202f2b4ced327a0c97dd2972f
Calliari <jrcalliari@me.com>
2022-06-15 19:58:35 +0100

    update s3 website module

--- main.tf
+++ main.tf
@@ -1,31 +1,43 @@
-# Terraform configuration
-
-resource "aws_s3_bucket" "s3_bucket" {
+resource "aws_s3_bucket" "this" {
   bucket        = var.bucket_name
-  force_destroy = true
   tags          = var.tag_map
-  acl           = "public-read"
-  policy        = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${var.bucket_name}/*"
-            ]
-        }
-    ]
+  force_destroy = true
 }
-EOF
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.this.arn,
+          "${aws_s3_bucket.this.arn}/*",
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_s3_bucket_website_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
   }
 }
