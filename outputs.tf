114b006eb3dc6e545a7cac8ee2413ab23608ca52
Calliari <jrcalliari@me.com>
2022-06-15 16:33:23 +0100

    s3 module working fine once its uncommented

--- outputs.tf
+++ outputs.tf
@@ -41,4 +41,29 @@ output "vpc-subnet-public-id" {
 output "vpc-subnet-private-id" {
   value = module.private_subnet.id.*
 }
+###########################
+# S3 outputs
+###########################
+# output "website_bucket_all" {
+#   description = "All info of the bucket"
+#   value       = module.website_s3_bucket.all
+# }
+# output "website_bucket_arn" {
+#   description = "ARN of the bucket"
+#   value       = module.website_s3_bucket.arn
+# }
 
+# output "website_bucket_name" {
+#   description = "Name (id) of the bucket"
+#   value       = module.website_s3_bucket.name
+# }
+
+# output "website_bucket_domain" {
+#   description = "Domain name of the bucket"
+#   value       = module.website_s3_bucket.domain
+# }
+
+# output "website_bucket_endpoint" {
+#   description = "Endpoint of the bucket"
+#   value       = module.website_s3_bucket.endpoint
+# }
