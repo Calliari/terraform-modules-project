9d0bbd7ab093fd336d4c46575b82f22286a22e55
Caliari Jr <Calliari@users.noreply.github.com>
2022-06-15 09:39:14 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -22,8 +22,9 @@ terraform {
 }
 
 ##################################################################################
-# PROVIDERS 
+# PROVIDERS with authentication method
 ##################################################################################
+# Variables taken from "terraform.tfvars" file
 provider "aws" {
   profile = var.profile
   region  = var.region != "" ? var.region : "us-east-2"
